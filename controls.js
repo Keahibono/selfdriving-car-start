2987e0183b52714cc1d8e2653d2b94545edd3420
Paul Keahi <paulkeahi@gmail.com>
2022-05-17 13:51:03 -1000

    Add files via upload
    drag and drop from yesterday's class

---
+++ controls.js
@@ -0,0 +1,48 @@
+class Controls{
+    constructor(){
+        this.forward = false;
+        this.reverse = false;
+        this.right = false;
+        this.left = false;
+
+        this.#addKeyboardListener();
+    }
+
+    #addKeyboardListener(){
+        document.onkeydown = (event) =>{
+            switch(event.key){
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+            }
+            //console.table(this)
+        }
+
+        document.onkeyup = (event) =>{
+            switch(event.key){
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+            }
+            //console.table(this)
+        }
+    }
+}
